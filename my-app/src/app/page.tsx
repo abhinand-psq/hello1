9973685fc93dc9b34948085873d4c2043545b6dc
abhinand-psq <103823872+abhinand-psq@users.noreply.github.com>
2024-10-26 13:59:36 +0530

    add video

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <iframe src="Recording 2024-10-24 183208.mp4" width={400} height={400} ></iframe>
+      <iframe src="file_example_MP4_480_1_5MG.mp4" width={400} height={400} ></iframe>
     </div>
   );
 }
