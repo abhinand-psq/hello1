1b8d67abd2f86823364061fa8120f4a5b67578b7
abhinand-psq <103823872+abhinand-psq@users.noreply.github.com>
2024-10-29 22:01:25 +0530

    changed video

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -2,8 +2,10 @@
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <iframe src="file_example_MP4_480_1_5MG.mp4" width={400} height={400} ></iframe>
+    <div className="h-screen">
+     <div className="h-screen flex justify-center items-center">
+     <iframe src="Recording 2024-10-24 183208.mp4" width={400} height={400} ></iframe>
+     </div>
     </div>
   );
 }
